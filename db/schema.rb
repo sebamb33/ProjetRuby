9e9c6e59963073c459122e3c1576e5a4eb7973d0
Colin Noiret <colin.noiret@gmail.com>
2022-06-14 22:10:22 +0200

    index homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-#ActiveRecord::Schema[7.0].define(version: 2022_06_14_131645) do
-
 ActiveRecord::Schema[7.0].define(version: 2022_06_14_133329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
