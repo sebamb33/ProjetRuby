e332fd39eb84fb034816afb2f61fa8bce9375a3c
sebastien <sebastienambona33@outlook.com>
2022-06-14 14:53:01 +0200

    Connexion à la base de données

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+# Création des  vols
+CSV.foreach(Rails.root.join('Sujet/flights.csv'), headers: true, col_sep: ';') do |row|
+  Vol.create({
+               numero: row['number'],
+               departure_aeroport: Aeroport.find_or_create_by(libelle: row['departure_airport']),
+               arrival_aeroport: Aeroport.find_or_create_by(libelle: row['arrival_airport']),
+               nb_place_business: row['business_class_seats'].to_i,
+               nb_place_economique: row['economy_class_seats'].to_i,
+               date_depart: row['departure_date'].to_datetime,
+               duree: row['duration'].to_i,
+             })
+end
