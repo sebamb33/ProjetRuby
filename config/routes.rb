dfd99737d56f528372bb2dd23d3b4f8af82d9cb7
sebastien <sebastienambona33@outlook.com>
2022-06-15 17:27:13 +0200

    partie reservations

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :vols
   resources :aeroports
   root 'vols#index'
+  get "reservations/new/:numVol",to:'reservations#new'
+  post "reservations/new/:numVol",to:'reservations#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
