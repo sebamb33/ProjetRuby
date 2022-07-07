672dde6f04f7146fed18c4303d79341d1f5a8e0a
sebastien <sebastienambona33@outlook.com>
2022-07-07 21:26:37 +0200

    partie reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,7 +3,7 @@ class ReservationsController < ApplicationController
 
   # GET /reservations or /reservations.json
   def index
-    @reservations = Reservation.all
+    @reservations = Reservation.where(user_id: current_user.id)
   end
 
   # GET /reservations/1 or /reservations/1.json
